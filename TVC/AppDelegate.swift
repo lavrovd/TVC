1c4366972321da2de7155e9fc6333f45a45ce2b4
Dmitry Lavrov <dmitry@formaltechnology.com>
2016-06-21 13:37:02 +0100

    Init Xcode project

---
+++ TVC/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TVC
+//
+//  Created by Dmitry Lavrov on 21/06/2016.
+//  Copyright © 2016 Formal Technology Ltd. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
